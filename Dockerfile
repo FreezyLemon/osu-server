b0675aca40aec4848576d491aa16656bd2d3cef1
FreezyLemon <freezestyleman@gmx.de>
2018-10-04 23:27:34 +0200

    add mysql connection retries

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,13 @@ WORKDIR /app
 
 COPY ./ .
 
-RUN dotnet build ./osu.Server.sln -c Release -v quiet
-RUN cp -r ./osu.Server.DifficultyCalculator/bin/Release/netcoreapp2.0/ ./build/
-
-WORKDIR /app/build
+RUN dotnet publish ./osu.Server.sln -c Release -v quiet
 
 FROM microsoft/dotnet:2.1-runtime-alpine
+
+WORKDIR /var/dotnet
+
+COPY --from=0 /app/osu.Server.DifficultyCalculator/bin/Release/netcoreapp2.1/publish/ ./
 
 # Not necessary for a dev server, but might be useful nonetheless
 # RUN apk update && apk upgrade
--- osu.Server.DifficultyCalculator/Database.cs
+++ osu.Server.DifficultyCalculator/Database.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu-server/master/LICENCE
 
+using System;
+using System.Threading;
 using MySql.Data.MySqlClient;
 
 namespace osu.Server.DifficultyCalculator
@@ -17,7 +19,22 @@ namespace osu.Server.DifficultyCalculator
         public MySqlConnection GetConnection()
         {
             var connection = new MySqlConnection(connectionString);
+
+            int retryCount = 0;
+            while (!connection.Ping())
+            {
+                if (++retryCount > 100)
+                {
+                    throw new InvalidOperationException("MySQL connection not available after a lot of retries!");
+                }
+
+                Console.WriteLine("MySQL Connection failed! Retrying in 5 seconds...");
+                // Wait 5 seconds and try again
+                Thread.Sleep(5000);
+            }
+
             connection.Open();
+            Console.WriteLine("Connected to MySQL database!");
             return connection;
         }
     }
